4195d961fa973fd089260498ed3cf8f26cb177dd
Candido Souza <candidosouzza@gmail.com>
2020-10-30 13:46:18 -0300

    testando as entidades

---
+++ encoder/tests/domain/video_test.go
@@ -0,0 +1,41 @@
+package tests_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateVideoIsEmpyty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIdIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "abc"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Nil(t, err)
+}
